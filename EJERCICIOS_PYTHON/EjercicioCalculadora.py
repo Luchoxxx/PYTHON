0671c1c1ffc54d1bc0d9db7352a8747f14d32ef3
Lucho <luisfernandezcardenas0@gmail.com>
2024-04-02 18:36:01 -0500

    Avance de Calculadora

--- EJERCICIOS_PYTHON/EjercicioCalculadora.py
+++ EJERCICIOS_PYTHON/EjercicioCalculadora.py
@@ -4,10 +4,9 @@ print("""
       Las operaciones que puedes realizar son : suma,resta,division, multiplicacion
       Para Salir escribe ("salir")
       """)
-bandera = 1
-cadena = []
+cadena = [] # 2,0 -> n + 0
 n = 0
-while bandera >0:
+while True:
     
     if len(cadena) == 0:
         numero = int(input("Ingresa un numero: "))
@@ -15,32 +14,36 @@ while bandera >0:
     operador = input("Elige la operacion: ")
 
     if len(cadena) > 0:
-       
+        resultado = 0
         if operador == "suma":
             numero1 = int(input("Ingresa el siguiente numero: "))
             resultado = cadena[n] + numero1
             print(f"El resultado es {resultado}")
 
-        if operador == "resta":
+        elif operador == "resta":
             numero1 = int(input("Ingresa el siguiente numero: "))
             resultado = cadena[n] - numero1
             print(f"El resultado es {resultado}")
 
-        if operador == "division":
+        elif operador == "division":
             numero1 = int(input("Ingresa el siguiente numero: "))
             resultado = cadena[n] / numero1
             print(f"El resultado es {resultado}")
 
-        if operador == "multiplicacion":
+        elif operador == "multiplicacion":
             numero1 = int(input("Ingresa el siguiente numero: "))
             resultado = cadena[n] * numero1
             print(f"El resultado es {resultado}")
-
+        elif  operador == "salir":
+            break
+        elif resultado == 0:
+            cadena.remove(resultado)    
+        else:
+            print("Entrada invalida")
+            if resultado == 0:
+                n = n - 1
         cadena.append(resultado)
         n = n +1
-    else:
-            print("Entrada invalida")
-    
-    if  operador == "salir":
-        break
+ 
+   
 
