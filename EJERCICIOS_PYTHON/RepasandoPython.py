367e92127f166f2aa63d5e5cc9e9c9191ffde483
Lucho <luisfernandezcardenas0@gmail.com>
2024-04-23 17:37:28 -0500

    Agreganado nuevo comentario una pequeña observacion

---
+++ EJERCICIOS_PYTHON/RepasandoPython.py
@@ -0,0 +1,14 @@
+
+## Tipo de variable set ## 
+
+my_set = {}
+my_set ={"Luis","Alberto",22}
+print(type(my_set))
+
+new_set = set()
+new_set.add("Direccion")
+print(type(new_set))
+print(new_set)
+print(len(my_set))
+
+
