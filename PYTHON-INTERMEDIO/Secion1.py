8c88e82110ac2c934fbafa0e447d12597b065320
Lucho <luisfernandezcardenas0@gmail.com>
2024-06-11 20:43:29 -0500

    cursos de la uni 1[A

--- PYTHON-INTERMEDIO/Secion1.py
+++ PYTHON-INTERMEDIO/Secion1.py
@@ -5,3 +5,8 @@ tupla1 = (1, 2, 3)
 
 print(type(tupla1))
 help(tupla1)
+
+#Tarea 
+#Considere y unicacamente los carcaracters de abecedario 
+#Cuente el numero de veces que aparece cada uno de estos caracters e un parrafo 
+#Almacenando el resultado en un diccionario
