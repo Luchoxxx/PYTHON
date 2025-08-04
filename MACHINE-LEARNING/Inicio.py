e1c1c522f4092fefbe00b4a0b4261fc2877e828c
Luis Alberto Fernandez Cardenas <luisfernandezcardenas0@gmail.com>
2025-08-04 14:53:08 -0500

    Añadiendo ejercicios de Programacion

--- MACHINE-LEARNING/Inicio.py
+++ MACHINE-LEARNING/Inicio.py
@@ -1,2 +1,21 @@
 
-print("hello world")
+'Primer Ejercicio de Python'
+
+contador3 = 0
+contador5 = 0
+contador = 0
+for i in range(1,101):
+    if i % 3  == 0:
+        contador3 += 1
+        print("fizz")
+    if i % 5 == 0:
+        print(i)
+        contador5 += 1
+        print("buzz")
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        contador += 1
+
+print(contador3)
+print(contador5)
+print(contador)
